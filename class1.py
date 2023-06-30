3fcadaaa2b7072bfa46d08eee7ebfe1ead8a51e5
富永隼太朗 <tominagashuntarou@tominagashuntarounoMacBook-Air.local>
2023-06-30 16:06:43 +0900

    newcommit

--- class1.py
+++ class1.py
@@ -56,7 +56,7 @@ def handle_message(event):
     message_text = event.message.text
     
     if '@mention' in message_text:
-        reply_text = "あいうえお"
+        reply_text = "aiueo"
     else:
         reply_text = "数字を入力してください。"
             
